790ce9bddde04fd863a4f47ff8c11c55a7e5af03
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2016-02-23 17:05:50 +0000

    Adding private member variables

--- app/src/main/java/uk/co/bigdogconsultants/funfacts/FunFactsActivity.java
+++ app/src/main/java/uk/co/bigdogconsultants/funfacts/FunFactsActivity.java
@@ -2,8 +2,14 @@ package uk.co.bigdogconsultants.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    // declare view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
